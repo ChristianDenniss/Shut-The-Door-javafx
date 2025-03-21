b995935cb37cf373a4e0cd3a11882f59c35b7e9a
Christian J. Dennis <166155393+ChristianDenniss@users.noreply.github.com>
2025-03-21 13:07:50 -0300

    Create Player.java
    Signed-off-by: Christian J. Dennis <166155393+ChristianDenniss@users.noreply.github.com>

---
+++ Player.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    private String username;
+    private int score;
+    
+    public Player(String username)
+    {
+        this.score = 45;
+        this.username = username;
+        
+    }
+    
+}
