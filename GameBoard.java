a8d20f9a9e47d98cf07d7b5cefd562da9c385945
Christian J. Dennis <166155393+ChristianDenniss@users.noreply.github.com>
2025-04-01 14:53:34 -0300

    Update GameBoard.java
    Signed-off-by: Christian J. Dennis <166155393+ChristianDenniss@users.noreply.github.com>

--- GameBoard.java
+++ GameBoard.java
@@ -10,21 +10,23 @@ public class GameBoard extends StackPane
 {
     private Text p1;
     private Text p2;
+    private ImageView boardBackGround;
     
     public GameBoard(Text p1, Text p2) 
     {
         this.p1 = p1;
         this.p2 = p2;
         
-        this.getChildren().addAll(p1, p2);
-        
         // Initialize the game screen with relevant UI elements
         //TileBoard player1Tiles = new TileBoard(true, 310, -50);
         //TileBoard player2Tiles = new TileBoard(false, 310, 50);
-        ImageView boardBackGround = MediaHandler.loadImage("boardBackGround.png", 1100, 750);
+        this.boardBackGround = MediaHandler.loadImage("boardBackGround.png", 1100, 750);
         
         // Add the components to the GameBoard layout (StackPane)
         this.getChildren().addAll(boardBackGround);//, player1Tiles, player2Tiles);
+        
+        //must come after board to not be over lapped, fixed mistake
+        this.getChildren().addAll(p1, p2);  
         
         boardBackGround.setMouseTransparent(true);
         p1.setMouseTransparent(true);
@@ -62,6 +64,7 @@ public class GameBoard extends StackPane
         // Log when the rotation is completed
         rotateTransition.setOnFinished(event -> {
             System.out.println("Rotation completed for GameBoard.");
+            
         });
     }
 }
